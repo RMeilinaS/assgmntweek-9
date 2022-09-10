1c152f8402e18d5e31664bb8b1876ae3c23a155c
RMeilinaS <meilinameilina110506@gmail.com>
2022-09-10 09:48:14 +0700

    heartrate

---
+++ main.py
@@ -0,0 +1,37 @@
+import paho.mqtt.client as mqtt
+from time import sleep
+
+from averageOksi import test_hr2
+
+# define static variable
+# broker = "localhost" # for local connection
+broker = "broker.hivemq.com"  # for online version
+port = 1883
+timeout = 60
+
+username = 'campuspedia'
+password = 'qlue'
+topic = "heartrate"
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+ 
+def on_publish(client,userdata,result):
+	print("data published \n")
+	
+
+
+client1 = mqtt.Client("nauval0")
+client1.username_pw_set(username=username,password=password)
+client1.on_connect = on_connect
+client1.on_publish = on_publish
+client1.connect(broker,port,timeout)
+
+test_hr2 = averageOksi ()
+
+status = float(test_hr2[0])
+print(status)
+
+for k in range(1,5):
+	ret = client1.publish(topic,payload=status)
+	sleep(1)
